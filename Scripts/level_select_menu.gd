88dfa6a338f3545f09265fd6cdd1f9d693a20b88
Jnight1104 <jnight1104@gmail.com>
2025-06-13 08:44:27 +1200

    Added placeholder menus, as well as player and level1 scenes

---
+++ Scripts/level_select_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Begins level 1 when it is pressed
+func _level_1_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
+
+
+# Navigation back to main menu when back is pressed
+func _back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main_menu.tscn")
---
+++ Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Navigation to the level select screen when play is pressed
+func _play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Level_select_menu.tscn")
+	
+
+# Quits the game when quit is pressed
+func _quit_button_pressed():
+	get_tree().quit()
