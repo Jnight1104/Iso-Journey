ccb726d6bdf82cef0c369b3faf05b8bf17810ca4
Jnight1104 <jnight1104@gmail.com>
2025-06-23 12:49:10 +1200

    Implemented successful player movement with control buffering

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,9 +5,12 @@ const UP : Vector3 = Vector3(1, 0, 0)
 const DOWN : Vector3 = Vector3(-1, 0, 0)
 const LEFT : Vector3 = Vector3(0, 0, -1)
 const RIGHT : Vector3 = Vector3(0, 0, 1)
+const MOVE_DELAY : float = 0.1
+const LERP_RATE : float = 0.4
+const MAX_ACTION_QUEUE : int = 1
 var target_location : Vector3 = SPAWN
-var target_distance : Vector3 = Vector3(0, 0, 0)
 var moving : bool = false
+var action_queue : Array = []
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,17 +27,28 @@ func _process(delta):
 	elif Input.is_action_just_pressed("ui_left"):
 		_move(LEFT)
 	elif Input.is_action_just_pressed("ui_right"):
-		_move(RIGHT)	
-	if position == target_location:
-		moving = false
-	print(lerp(position, target_location, 0.4))
-	target_distance = abs(target_location - position)
-	if abs(lerp(position, target_location, 0.4)).x < 0.001 and abs(lerp(position, target_location, 0.4)).z < 0.001:
-		position = target_location
-	position = lerp(position, target_location, 0.4)
+		_move(RIGHT)
+	# Moves the player toward the target location smoothly
+	position = lerp(position, target_location, LERP_RATE)
 
+
+# Function for setting the target location depending on the direction based on the input
 func _move(direction):
-	if not moving:
+	if moving:
+		if len(action_queue) < MAX_ACTION_QUEUE:
+			action_queue.append(direction)
+	else:
 		moving = true
 		target_location += direction
-		
+		$Move_delay_timer.start(MOVE_DELAY)
+
+
+# Allows player to move again after the delay is done
+func _move_delay_done():
+	# Repeats movement if the action queue isnt done
+	if len(action_queue) > 0:
+		target_location += action_queue[0]
+		action_queue.remove_at(0)
+		$Move_delay_timer.start(MOVE_DELAY)
+	else:
+		moving = false
