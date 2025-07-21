00068eddc9e3ec792de524fd3da2fa8b609cbbff
Jnight1104 <jnight1104@gmail.com>
2025-07-21 13:33:59 +1200

    adjusted level identifier, created level 2 scene

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,5 +1,5 @@
 extends Node
 
 const PLAYER_STARTING_POS_Y : float = 0.5
-var level : int = 1
+var level : int = 0
 var player_pos : Vector3 = Vector3(0, PLAYER_STARTING_POS_Y, 0)
--- Scripts/level_select_menu.gd
+++ Scripts/level_select_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var global = get_node("/root/Global")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +14,7 @@ func _process(delta):
 
 # Begins level 1 when it is pressed
 func _level_1_pressed():
+	global.level = 1
 	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
 
 
