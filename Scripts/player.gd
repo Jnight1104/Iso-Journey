36c74762de318f797763e87f32c83a22b5717e46
Jnight1104 <jnight1104@gmail.com>
2025-07-28 13:29:58 +1200

    further implementing undo system

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,7 +11,7 @@ const LERP_RATE : float = 12
 const MAX_ACTION_QUEUE : int = 2
 const OBSTACLE_BUFFER_SCALE : float = 0.3
 const UNDO_OFFSET : int = 1
-const UNDO : String = "undo"
+const UNDO : Vector3 = Vector3(-100, 0, 0)
 var target_location : Vector3 = SPAWN
 var moving : bool = false
 var action_queue : Array = []
@@ -36,7 +36,9 @@ func _ready():
 func _process(delta):
 	global.player_pos = position
 	if not game_finished and not global.paused:
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_minus"):
+			_move_input(UNDO)
+		elif Input.is_action_just_pressed("ui_up"):
 			_move_input(UP)
 		elif Input.is_action_just_pressed("ui_down"):
 			_move_input(DOWN)
@@ -44,11 +46,7 @@ func _process(delta):
 			_move_input(LEFT)
 		elif Input.is_action_just_pressed("ui_right"):
 			_move_input(RIGHT)
-	if Input.is_action_just_pressed("ui_minus"):
-		if undos < len(action_history) - UNDO_OFFSET:
-			action_queue = []
-			undos += UNDO_OFFSET
-			target_location = action_history[len(action_history) - (undos + UNDO_OFFSET)]
+	# Clears pending actions when game is paused
 	if Input.is_action_just_pressed("ui_escape"):
 		action_queue = []
 	# Moves the player toward the target location smoothly
@@ -66,33 +64,41 @@ func _move_input(direction):
 
 
 func _move(direction):
-	action_history = action_history.slice(0, )
-	if direction == UP:
-		detection_ray = $Forward
-	elif direction == RIGHT:
-		detection_ray = $Right
-	elif direction == DOWN:
-		detection_ray = $Back
-	elif direction == LEFT:
-		detection_ray = $Left
-	if detection_ray.is_colliding():
-		if detection_ray.get_collider().is_in_group("Box"):
-			#push.connect(detection_ray.get_collider()._pushed)
-			push.emit(direction, detection_ray.get_collider())
-			action_history.append(target_location)
+	if direction == UNDO:
+		if undos < len(action_history) - UNDO_OFFSET:
+			undos += UNDO_OFFSET
+			target_location = action_history[len(action_history) - (undos + UNDO_OFFSET)]
 			$Move_delay_timer.start(MOVE_DELAY)
-			position += direction * OBSTACLE_BUFFER_SCALE
-		elif detection_ray.get_collider().is_in_group("Boundaries"):
-			$Move_delay_timer.start(MOVE_DELAY)
-			position += direction * OBSTACLE_BUFFER_SCALE
+	else:
+		if undos > 0:
+			action_history = action_history.slice(0, len(action_history) - undos)
+			undos = 0
+		if direction == UP:
+			detection_ray = $Forward
+		elif direction == RIGHT:
+			detection_ray = $Right
+		elif direction == DOWN:
+			detection_ray = $Back
+		elif direction == LEFT:
+			detection_ray = $Left
+		if detection_ray.is_colliding():
+			if detection_ray.get_collider().is_in_group("Box"):
+				#push.connect(detection_ray.get_collider()._pushed)
+				push.emit(direction, detection_ray.get_collider())
+				action_history.append(target_location)
+				$Move_delay_timer.start(MOVE_DELAY)
+				position += direction * OBSTACLE_BUFFER_SCALE
+			elif detection_ray.get_collider().is_in_group("Boundaries"):
+				$Move_delay_timer.start(MOVE_DELAY)
+				position += direction * OBSTACLE_BUFFER_SCALE
+			else:
+				target_location += direction
+				action_history.append(target_location)
+				$Move_delay_timer.start(MOVE_DELAY)
 		else:
 			target_location += direction
 			action_history.append(target_location)
 			$Move_delay_timer.start(MOVE_DELAY)
-	else:
-		target_location += direction
-		action_history.append(target_location)
-		$Move_delay_timer.start(MOVE_DELAY)
 		
 
 # Allows player to move again after the delay is done
