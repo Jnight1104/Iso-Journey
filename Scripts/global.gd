ab1ddda937cb70d3a936c937ee22237bd6d2fb9a
Jnight1104 <jnight1104@gmail.com>
2025-08-01 09:28:37 +1200

    Implemented level 2 pressure plate functionality

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,4 +3,5 @@ extends Node
 const PLAYER_STARTING_POS_Y : float = 0.5
 var level : int = 0
 var player_pos : Vector3 = Vector3(0, PLAYER_STARTING_POS_Y, 0)
-var paused = false
+var paused : bool = false
+var objectives_reached : int = 0
--- Scripts/pressure_plate.gd
+++ Scripts/pressure_plate.gd
@@ -8,7 +8,8 @@ signal release_signal
 func _ready():
 	$Unpressed.show()
 	$Pressed.hide()
-	if global.level == 1:
+	global.objectives_reached = 0
+	if global.level == 1 or global.level == 2:
 		press_signal.connect(get_node("/root/Level/Win_collectible")._unlocked)
 		release_signal.connect(get_node("/root/Level/Win_collectible")._locked)
 
@@ -16,10 +17,21 @@ func _ready():
 func _pressed(_body):
 	$Pressed.show()
 	$Unpressed.hide()
-	press_signal.emit()
+	if global.level == 1:
+		press_signal.emit()
+	elif global.level == 2:
+		global.objectives_reached += 1
+		print(global.objectives_reached)
+		if global.objectives_reached == 19:
+			press_signal.emit()
 
 
 func _released(_body):
 	$Unpressed.show()
 	$Pressed.hide()
-	release_signal.emit()
+	if global.level == 1:
+		release_signal.emit()
+	elif global.level == 2:
+		global.objectives_reached -= 1
+		print(global.objectives_reached)
+		release_signal.emit()
