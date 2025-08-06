6d47d70b7a3771b4bfe52bca827a0f1abeb9ffe9
Jnight1104 <jnight1104@gmail.com>
2025-08-06 22:43:47 +1200

    Began inplementing level select screen UI

--- Scripts/level_select_menu.gd
+++ Scripts/level_select_menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
 @onready var global = get_node("/root/Global")
+const TRANSPARENT : Color = Color(1, 1, 1, 1)
+const DARKENED : Color = Color(0.9, 0.9, 0.9, 1)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Back.set_modulate(TRANSPARENT)
 
 
 # Begins level 1 when it is pressed
@@ -21,3 +24,11 @@ func _level_2_pressed():
 # Navigation back to main menu when back is pressed
 func _back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Main_menu.tscn")
+
+
+func _back_mouse_entered():
+	$Back.set_modulate(DARKENED)
+
+
+func _back_mouse_exited():
+	$Back.set_modulate(TRANSPARENT)
