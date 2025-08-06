2c8f1dbab343d0b2d26fea3f67a2aa207b2eabfa
Jnight1104 <jnight1104@gmail.com>
2025-08-06 21:54:21 +1200

    Implemented options menu UI

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,10 +1,21 @@
 extends Control
 
+const TRANSPARENT : Color = Color(1, 1, 1, 1)
+const DARKENED : Color = Color(0.9, 0.9, 0.9, 1)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Back.set_modulate(TRANSPARENT)
 
 
 func _back_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Main_menu.tscn")
+
+
+func _back_mouse_entered():
+	$Back.set_modulate(DARKENED)
+
+
+func _back_mouse_exited():
+	$Back.set_modulate(TRANSPARENT)
